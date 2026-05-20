dc64ad64c5822a295a14586683f28b660bca0c52
rithvik <rithvikmittapalli10@gmial.com>
2026-05-20 23:24:58 +0530

    Add SimpleJWT token refresh route to urls.py

--- finance_ai/finance_ai/urls.py
+++ finance_ai/finance_ai/urls.py
@@ -18,6 +18,7 @@ from advisor.views import (
     signup,
     login
 )
+from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
@@ -32,6 +33,7 @@ urlpatterns = [
     path('signup/', signup),
     path('login/', login),
     path('me/', get_me),
+    path('token/refresh/', TokenRefreshView.as_view()),
     path('advisor/', include('advisor.urls')),
 
     # -------------------------------
